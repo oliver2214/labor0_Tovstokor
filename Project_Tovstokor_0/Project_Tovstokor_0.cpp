ef060e9e541561ec2f93ee0fef2ce655b6239bb3
oliver2214 <dima.tovstokor@yandex.ru>
2021-10-08 21:07:44 +0300

    Hello world

---
+++ Project_Tovstokor_0/Project_Tovstokor_0.cpp
@@ -0,0 +1,15 @@
+
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    if (a == 1)
+        cout << "Hello World!!!";
+    return 0;
+}
